349d57a3066eba92c64033222203010b387fd748
dawudu <maila.manthole@gmail.com>
2022-06-30 14:30:55 -0700

    Fixed Syntax

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
+
 /**
- *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 
