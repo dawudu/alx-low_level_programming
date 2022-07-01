bd03431154ff0be1e6999a44809936087a353a2f
dawudu <maila.manthole@gmail.com>
2022-07-01 15:13:06 -0700

    commit 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - prints alphabet in lowercase
+*followed by a new line
+*Result:Always 0
+*/
+int main(void)
+
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+ch++;
+}
+putchar('\n');
+return (0);
+}
