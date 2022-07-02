0e3c6cc7bc9f101bfc799db1616fa4cc7e30e608
dawudu <maila.manthole@gmail.com>
2022-07-01 17:50:14 -0700

    Added fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,14 +8,11 @@
 int main(void)
 
 {
-	int n;
-
-	for (n = 48; n < 58; n++)
-	{
-		putchar(n);
-	}
-
-	putchar('\n');
-
-	return (0);
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
 }
