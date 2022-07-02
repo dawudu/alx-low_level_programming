11516452178d93e1c84a3e6270d961bc5b786ecb
dawudu <maila.manthole@gmail.com>
2022-07-01 17:19:02 -0700

    FIxed error

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-*main - a program that prints all single digit numbers of base 10 starting from 0
+*main - a program that prints all single digit numbers
 *followed by anew line 
 *Return: Always 0
 */
