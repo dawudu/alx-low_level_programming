e87e0deab1fbb0d2a6b0e6760d3a77ce28c150f9
dawudu <maila.manthole@gmail.com>
2022-07-06 03:46:52 -0700

    commit 0 error

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,35 +1,7 @@
+#include "main.h"
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
-
-
-
-void positive_or_negative(int i)
-{
-
-int n;
-
-srand(time(0));
-
-n = i;
-
-if (n > 0)
-
-printf("%d is positive\n", n);
-
-else if (n == 0)
-
-printf("%d is zero\n", n);
-
-else if (n < 0)
-
-printf("%d is negative\n", n);
-
-return (0);
-
-}
 
 /**
 * main - debugging example
