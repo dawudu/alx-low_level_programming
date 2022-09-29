e23ae7886b1e1197632092527d37f9e4da920061
dawudu <maila.manthole@gmail.com>
2022-09-29 10:42:28 -0700

    commit 8 fixed

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,47 +1,47 @@
 #include "lists.h"
 /**
-* delete_dnodeint_at_index - deletes the node at index index of a list
-* @head: pointer to the head pointer of the list
-* @index: is the index of the node that should be deleted
+* delete_dnodeint_at_index - deletes the node at index of a
+* dlistint_t linked list
+* @head: head of the list
+* @index: index of the new node
 * Return: 1 if it succeeded, -1 if it failed
-* Description: function that deletes the node at index index of a list
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *del;
-	unsigned int i = 0;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-	if (!*head)
-		return (-1);
-	del = *head;
-	if (index == 0)
+	h1 = *head;
+
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
+
+	i = 0;
+
+	while (h1 != NULL)
 	{
-		if (del->next)
+		if (i == index)
 		{
-			*head = del->next;
-			del->next->prev = NULL;
-		}
-		else
-		{
-			*head = NULL;
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+			free(h1);
 			return (1);
 		}
+		h2 = h1;
+		h1 = h1->next;
+		i++;
 	}
-	else
-	{
-		while (i < index)
-		{
-			i++;
-			if ((del->next == NULL) && (i != (index - 2)))
-				return (-1);
-			del = del->next;
-		}
-		del->prev->next = del->next;
-		if (del->next)
-			del->next->prev = del->prev;
-		else
-			del->prev->next = NULL;
-	}
-	free(del)
-	return (1);
+	return (-1);
 }
